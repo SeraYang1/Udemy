90a3ea951dfb2ff11eed124d2913589f2f3de11f
Sera Yang <sera.yang@berkeley.edu>
2017-06-20 00:17:50 -0700

    day 2

---
+++ NodeJS/Section-3/notes.js
@@ -0,0 +1,11 @@
+console.log('starting notes');
+
+//exports is basically the only useful thing, use other classes variables/functions
+module.exports.addNote = () => {
+  console.log('addNode');
+  return ('new Node');
+};
+
+module.exports.add = (a, b) => {
+  return a + b;
+}
