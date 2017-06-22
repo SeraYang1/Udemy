8bf5d07d38c2e44c62d95ef4c120c0ac227176ee
Sera Yang <sera.yang@berkeley.edu>
2017-06-21 19:25:05 -0700

    lesson 14

--- NodeJS/Section-3/app.js
+++ NodeJS/Section-3/app.js
@@ -2,6 +2,25 @@ console.log('starting app');
 
 //loads in the  module with require, https://nodejs.org/api/
 const fs = require('fs');
-const lodash = require('lodash')
+const lodash = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
+
+
+var command = process.argv[2];
+var args = yargs.argv
+console.log("process", process.argv)
+console.log("yargs", yargs.argv)
+
+if (command === 'add'){
+  notes.addNote(args.title, args.body)
+} else if (command === 'list'){
+  notes.listNotes()
+}else if (command === 'read'){
+  notes.readNote(args.title)
+}else if (command === 'remove'){
+  notes.removeNote(args.title)
+}else{
+  console.log("Command not recognized")
+}
--- NodeJS/Section-3/notes.js
+++ NodeJS/Section-3/notes.js
@@ -1,7 +1,22 @@
 console.log('starting notes');
 
 //exports is basically the only useful thing, use other classes variables/functions
-module.exports.addNote = () => {
-  console.log('addNode');
-  return ('new Node');
+var addNote = (title, body) => {
+  console.log("adding note ", title, body)
 };
+var listNotes = () => {
+  console.log("Listing note ")
+};
+var readNote = (title) => {
+  console.log("Reading note ", title)
+};
+var removeNote = (title) => {
+  console.log("Removing note ", title)
+};
+
+module.exports = {
+  addNote,
+  listNotes,
+  readNote,
+  removeNote
+}
