6badc170601fa8165dd36f1301652baa9d941e12
Sera Yang <sera.yang@berkeley.edu>
2017-07-01 10:47:20 -0700

    section 4 challenge

--- NodeJS/Section-4/app-promise.js
+++ NodeJS/Section-4/app-promise.js
@@ -1,14 +1,15 @@
 const yargs = require('yargs');
-const geocode = require('./geocoder/geocoder.js')
-const weather = require('./weather/weather.js')
+//const geocode = require('./geocoder/geocoder.js')
+//const weather = require('./weather/weather.js')
 const axios = require('axios')
+const fs = require('fs')
 
 var argv = yargs
 	.options({
-		a: {
+		address: {
 			describe: 'Address to fetch weather from',
-			demand: true,
-			alias: 'address',
+			demand: false,
+			alias: 'a',
 			string: true
 		}
 	})
@@ -17,27 +18,40 @@ var argv = yargs
 	.alias('help', 'h')
 	.argv;
 
-var encoded = encodeURIComponent(argv.address)
-var codedURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encoded
+
+if (argv.address) {
+	var encoded = encodeURIComponent(argv.address)
+	var codedURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encoded
+	fs.writeFileSync('location.json', JSON.stringify(argv.address));
+}else{
+	try {
+		var noteString = fs.readFileSync('location.json');
+		var encoded = encodeURIComponent(noteString)
+		var codedURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + encoded
+	} catch (e) {
+		console.log("You need to enter an address!")
+	}
+}
 
 axios.get(codedURL).then((response) => {
-	if(response.data.status === 'ZERO_RESULTS'){
+	if (response.data.status === 'ZERO_RESULTS') {
 		throw new Error('Unable to find that address.');
 	}
 
 	var lat = response.data.results[0].geometry.location.lat;
 	var long = response.data.results[0].geometry.location.lng;
-	var weatherURL = 'https://api.darksky.net/forecast/dc0f66d65ed468a6581dcfd18379fdb0/'+lat+','+long
+
+	var weatherURL = 'https://api.darksky.net/forecast/dc0f66d65ed468a6581dcfd18379fdb0/' + lat + ',' + long
 	console.log(response.data.results[0].formatted_address)
 	return axios.get(weatherURL)
 }).then((response) => {
 	var temp = response.data.currently.temperature;
 	var apparentTemp = response.data.currently.apparentTemperature;
 	console.log(`It is currently ${temp} but feels like ${apparentTemp}`);
-}).catch((e) =>{
-	if(e.code === 'ENOTFOUND'){
+}).catch((e) => {
+	if (e.code === 'ENOTFOUND') {
 		console.log('Unable to connect to API services');
-	}else{
+	} else {
 		console.log(e.message);
 	}
 })
