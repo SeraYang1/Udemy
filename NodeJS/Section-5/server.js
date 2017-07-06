91cf79f1b9f38e380a73c6a64eddea766fbe4e14
Sera Yang <sera.yang@berkeley.edu>
2017-07-06 09:35:37 -0700

    lesson 45, gitignore

--- NodeJS/Section-5/server.js
+++ NodeJS/Section-5/server.js
@@ -1,38 +1,55 @@
 const express = require('express')
 const hbs = require('hbs')
+const fs = require('fs')
 var app = express();
 
 app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'hbs')
 
+app.use((req, res, next) => {
+	var log = `${new Date().toString()}: ${req.method} ${req.originalUrl}`
+	fs.appendFile('server.log', log + `\n`, (err) => {
+		if (err){
+			console.log('Unable to append to server.log')
+		}
+	})
+	console.log(log)
+	next();
+})
+
+app.use((req, res, next) => {
+	res.render('maintenance.hbs')
+})
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+	return text.toUpperCase();
+})
+
 app.get('/', (req, res) => {
-  // res.send({
-  //   name: 'Sera Yang',
-  //   hobbies: [
-  //     'Longboarding',
-  //     'Traveling'
-  //   ]
-  // });
-  res.render('home.hbs',{
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear(),
-    welcomeText: "Hello!"
-  })
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		welcomeText: "Hello!"
+	})
 });
 
 app.get('/about', (req, res) => {
-  res.render('about.hbs',{
-    pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
-  })
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	})
 })
 
 app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage: 'Error!!!'
-  })
+	res.send({
+		errorMessage: 'Error!!!'
+	})
 })
 
 app.listen(3000, () => {
-  console.log("Running port")
+	console.log("Running port")
 });
