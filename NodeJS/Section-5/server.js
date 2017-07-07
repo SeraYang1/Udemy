96d45807c6727613a5e75f8cf0ce7920ee943767
Sera Yang <sera.yang@berkeley.edu>
2017-07-07 11:09:01 -0700

    janky handlebars

--- NodeJS/Section-5/server.js
+++ NodeJS/Section-5/server.js
@@ -18,19 +18,17 @@ app.use((req, res, next) => {
 	next();
 })
 
-app.use((req, res, next) => {
-	res.render('maintenance.hbs')
-})
-
 hbs.registerPartials(__dirname + '/views/partials')
 
 hbs.registerHelper('getCurrentYear', () => {
 	return new Date().getFullYear()
 })
 
-hbs.registerHelper('screamIt', (text) => {
-	return text.toUpperCase();
+hbs.registerHelper('fetchWeather', (text) => {
+	console.log(text)
+	return;
 })
+
 
 app.get('/', (req, res) => {
 	res.render('home.hbs', {
@@ -39,17 +37,11 @@ app.get('/', (req, res) => {
 	})
 });
 
-app.get('/about', (req, res) => {
-	res.render('about.hbs', {
-		pageTitle: 'About Page'
-	})
-})
-
-app.get('/bad', (req, res) => {
-	res.send({
-		errorMessage: 'Error!!!'
-	})
-})
+// app.get('/about', (req, res) => {
+// 	res.render('about.hbs', {
+// 		pageTitle: 'About Page'
+// 	})
+// })
 
 app.listen(port, () => {
 	console.log(`Server is up on port: ${port}`)
