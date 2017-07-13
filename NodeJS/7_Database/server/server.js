e790754ed9ff7dd79eba4cbd4d77a32c52a6ea5a
Sera Yang <sera.yang@berkeley.edu>
2017-07-12 23:35:28 -0700

    lesson 75

--- NodeJS/7_Database/server/server.js
+++ NodeJS/7_Database/server/server.js
@@ -1,9 +1,15 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 
-var {mongoose} = require('./db/mongoose')
-var {Todo} = require('./models/todo')
-var {User} = require('./models/users')
+var {
+	mongoose
+} = require('./db/mongoose')
+var {
+	Todo
+} = require('./models/todo')
+var {
+	User
+} = require('./models/users')
 
 var app = express();
 
@@ -11,19 +17,32 @@ var app = express();
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-  //only creates a documnet, doesn't actually save it yet
-  var newTodo = new Todo({
-    text: req.body.text
-  });
+	//only creates a documnet, doesn't actually save it yet
+	var newTodo = new Todo({
+		text: req.body.text
+	});
 
-  //don't need the promise, only include it if you want to do something after saving it
-  newTodo.save().then((doc) => {
-    res.send(doc)
-  }, (e) => {
-    res.send(e);
-  });
+	//don't need the promise, only include it if you want to do something after saving it
+	newTodo.save().then((doc) => {
+		res.send(doc)
+	}, (e) => {
+		res.status(400).send(e);
+	});
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000')
+app.get('/todos', (req, res) => {
+	Todo.find().then((success) => {
+		res.send({
+			success
+		})
+    console.log(JSON.stringify(success, undefined, 2))
+	}, (fail) => res.status(400).send(fail))
 })
+
+app.listen(3000, () => {
+	console.log('Started on port 3000')
+})
+
+module.exports = {
+	app
+}
